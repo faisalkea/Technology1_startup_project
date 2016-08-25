7872d04265fd79fc5c1d81da856e641b36aff147
Faisal Jarkass <faisaljarkass@Faisals-MacBook-Pro.local>
2016-08-25 21:14:39 +0200

    Initial commit

---
+++ src/main/java/com/faisal/MyServlet.java
@@ -0,0 +1,70 @@
+package com.faisal;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by faisaljarkass on 19/08/16.
+ */
+public class MyServlet extends HttpServlet {
+
+    private static Logger logger = Logger.getLogger(MyServlet.class.getName());
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.log(Level.INFO, "doPost start...");
+        logger.log(Level.INFO, "Username: " + request.getParameter("username"));
+        logger.log(Level.INFO, "Password: " + request.getParameter("password"));
+        logger.log(Level.INFO, "Checkbox: " + request.getParameter("rememberMe"));
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Using POST Method to Read Form Data";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>Username</b>: "
+                + request.getParameter("username") + "\n" +
+                "  <li><b>Password</b>: "
+                + request.getParameter("password") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
+        logger.log(Level.INFO, "doPost ended...");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.log(Level.INFO, "doGet started...");
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Using POST Method to Read Form Data";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>Username</b>: "
+                + request.getParameter("username") + "\n" +
+                "  <li><b>Password</b>: "
+                + request.getParameter("password") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
+        logger.log(Level.INFO, "doGet ended...");
+    }
+}
---
+++ src/test/java/com/faisal/MyServletTest.java
@@ -0,0 +1,21 @@
+package com.faisal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by faisaljarkass on 25/08/16.
+ */
+public class MyServletTest {
+
+    @Test
+    public void testDoPost() throws Exception {
+
+    }
+
+    @Test
+    public void testDoGet() throws Exception {
+
+    }
+}
